b55ceffc48f5b155a23f97eeb571903146d13a89
Hanzhizheng <304292903@qq.com>
2018-01-31 14:53:54 +0800

    ci run_or_fail.sh update_repo.sh

--- ci/code/update_repo.sh
+++ ci/code/update_repo.sh
@@ -35,3 +35,11 @@ if [ $NEW_COMMIT_ID != $COMMIT_ID ]; then
   popd 1> /dev/null
   echo $NEW_COMMIT_ID > .commit_id
 fi
+
+
+# source: python import
+# pushd A: 把指定目录A压入目录栈，同时切换到A目录，再次pushd, 将栈顶的A目录和栈顶下一目录B对调，切换到B目录
+# 2> /dev/null 代表忽略掉错误信息, 1> /dev/null 把标准输出重定向到/dev/null，其实就是屏蔽标准输出
+# git log -nx: 最后x个log
+# awk '{ print $2 }: 打印第二个字段
+# popd: 与pushd相反，弹出，切换到栈顶的下一个目录
